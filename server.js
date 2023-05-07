7440127660f375c2c82d1ec052203051b2726856
Kate <kcbryan@email.unc.edu>
2023-05-07 18:14:01 -0400

    fix behavior for undefined endpoint

--- server.js
+++ server.js
@@ -14,12 +14,7 @@ if (port==null) {
 
 app.use(cors());
 
-// oprq2
-app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.status(404).send("404 NOT FOUND");
-    
-  });
+
 
 //oprq3
 app.get('/app/', (req, res) => {
@@ -90,6 +85,11 @@ app.get('/app/rpsls/play/lizard/', (req, res)=> {
 app.get('/app/rpsls/play/spock/', (req, res)=> {
     res.send(rpsls('spock'));
 })
+
+// oprq2
+app.use((req, res, next) => {
+    res.status(404).send("404 NOT FOUND")
+    })
 
 
 
